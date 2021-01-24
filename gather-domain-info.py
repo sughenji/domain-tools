b1170b21e512dbbef69574fc52ccb7b21eb45154
sugo <sugo@vm.sugodipesce.net>
2021-01-24 13:25:31 +0100

    add check for possible external mail services

--- gather-domain-info.py
+++ gather-domain-info.py
@@ -9,13 +9,27 @@ if len(sys.argv) != 2:
 w = whois.whois(sys.argv[1])
 ip = socket.gethostbyname((sys.argv[1]))
 
-print('Domain: ' + colored(str(w.domain_name), 'green') + '\n')
+print('Domain: ' + colored(str(sys.argv[1]), 'green') + '\n')
 print('Expiration date: ' + colored(str(w.expiration_date), 'green') + '\n')
 print('Authoritative name server:\n' + colored(str(w.name_servers), 'green') + '\n')
-print('DNS Lookup: www.' + str(w.domain_name) + ' points to: ' + colored(socket.gethostbyname('www.' +str(w.domain_name)), 'green'))
-print('DNS Lookup: ' + str(w.domain_name) + ' points to: ' + colored(socket.gethostbyname(str(w.domain_name)), 'green') + '\n')
+print('DNS Lookup: www.' + str(sys.argv[1]) + ' points to: ' + colored(socket.gethostbyname('www.' +str(sys.argv[1])), 'green'))
+print('DNS Lookup: ' + str(sys.argv[1]) + ' points to: ' + colored(socket.gethostbyname(str(sys.argv[1])), 'green') + '\n')
 print('Mail Routing:\n')
-mx = dns.resolver.query(w.domain_name, 'MX')
+mx = dns.resolver.query(sys.argv[1], 'MX')
 for rdata in mx:
     print('MX:', colored(rdata.exchange, 'green'), 'priority:', colored(rdata.preference, 'green'))
-print('\nThis domain seems hosted on server: ' + colored(str(socket.gethostbyaddr(ip)[0]), 'green'))
+# Common third domain level for mail services
+mailservices = [ 'mail.' ]
+mailservicesip = []
+
+for item in mailservices:
+        if socket.gethostbyname(str(item) + str(sys.argv[1])):
+                mailservicesip.append(socket.gethostbyname(str(item) + str(sys.argv[1])))
+
+for x in mailservicesip:
+	if str(ip) != str(x):
+		print(colored('NOTICE: mail services seem hosted on: ' + str(x), 'red'))
+		exit('\nFinished.\n')
+	else:
+		print('\nThis domain seems hosted on server: ' + colored(str(socket.gethostbyaddr(ip)[0]), 'green'))
+print('\nFinished.\n')
